f2750d635e1ca199fadf230b215728b39a7917bc
Bogdan <bveretelnik@gmail.com>
2020-10-19 18:27:38 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import FirebaseState from './components/context/firebase/FirebaseState';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
+    <FirebaseState>
     <div className="todo-app">
       <TodoList/>
     </div>
+    </FirebaseState>
   );
 }
 
--- src/components/context/firebase/FirebaseState.js
+++ src/components/context/firebase/FirebaseState.js
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+import axios from 'axios'
+import { FirebaseContext } from './firebaseContext'
+import { ADD_TODO, FETCH_TODOS, REMOVE_TODO, RENAME_TODO } from "../types"
+import { firebaseReducer } from './firebaseReducer'
+import Axios from 'axios'
+
 
 
 const url = 'https://react-todos-hooks-63786.firebaseio.com'
 
-export default function FirebaseState() {
+export default function FirebaseState({children}) {
 
+    const initialState = {
+        notes: []
+    }
 
+const [state, dispatch] = useReducer(firebaseReducer, initialState)
+
+const fetchTodos = async() =>{
+
+    const res = await axios.get(`${url}/todos.json`)
+
+    const payload = Object.keys(res.data).map(key => {
+        return {
+            ...res.data[key],
+            id:key
+        }
+    })
+    dispatch({
+        type:FETCH_TODOS,
+        payload
+    })
+}
+
+    const addTodo = async text => {
+        //......
+    }
+
+    const removeTodo = async id => {
+        await axios.delete(`${url}/todos/${id}.json`)
+        dispatch({
+            type:RENAME_TODO,
+            payload:id
+        })
+    }
+
+    const renameTodo = id => {
+        //.....
+    }
 
     return (
-        <div>
-            
-        </div>
+        <FirebaseContext.Provider value={{
+            fetchTodos,addTodo,removeTodo,renameTodo,
+            todos:state.todos
+        }}>
+            {children}
+        </FirebaseContext.Provider>
     )
 }
